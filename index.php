f6e38baaa4b3e978cbc0ee01cd3ddc3570eb806e
Antoine Pouyollon <antoine.pouyollon@gmail.com>
2024-01-31 15:51:39 +0100

    updated_headers&footers

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 
-include 'jeux.php';
-include 'rating.php';
-include 'tag.php';
-include 'user.php';
+include 'src/jeux.php';
+include 'src/rating.php';
+include 'src/tag.php';
+include 'src/user.php';
 
 ?>
---
+++ templates/header.php
@@ -0,0 +1,24 @@
+
+<!-- Header -->
+    <header class="header">
+        <section id="section-logo">
+            <img id="logo" src="assets/" alt="" />
+        </section>
+
+        <nav class="nav-bar">           
+            <ul class="menu-nav">
+                <li class="menu-nav__item">
+                <a href="index.php" class="menu-nav__link">#</a>
+                </li>
+                <li class="menu-nav__item">
+                <a href="#" class="menu-nav__link">#</a>
+                </li>
+                <li class="menu-nav__item">
+                <a href="#" class="menu-nav__link">#</a>
+                </li>
+            </ul>
+        </nav> 
+
+        <div class="span-log"></div>
+        
+    </header>
