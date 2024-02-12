35f55dc8fce717164de80eb9ee368f2d6ea93f9a
archiMydi <archibald.venzal@my-digital-school.org>
2024-02-12 13:47:35 +0100

    Modif fichier connexion à bdd (PDO au lieu de mysqli)

--- src/templates/connection.inc.php
+++ src/templates/connection.inc.php
@@ -3,9 +3,15 @@
 //A déplacer à la racine du projet une fois rempli
 //Ne pas envoyer sur le github une fois rempli
 
-$servername = "mysql2.ouiheberg.com:3306";
+$servername = "mysql2.ouiheberg.com";
+$port = "3306";
 $username = "u3377_NwoQzma0Jn";
 $password = "5jvxEBJ@jRNLcZlCYMj.d3Hs";
 $dbname = "s3377_jadenn";
-$conn = new mysqli($servername, $username, $password, $dbname);
-?>
+
+try {
+    $conn = new PDO("mysql:host=$servername;port=$port;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Erreur de connexion : " . $e->getMessage();
+}
---
+++ static/js/user_list.js
@@ -0,0 +1,8 @@
+//Un user a les attributs suivant à afficher : Pseudo, Description, Photo, Jeu préféré
+
+let userTable;
+
+function userTableCreation() { //Créer un array pour le stockage local des users
+
+}
+
