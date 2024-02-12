db2df05a32abaee897fe345ad6c338cdd35659ad
Game Black <71898409+GameBlackRC@users.noreply.github.com>
2024-02-12 13:53:23 +0100

    Update connection.inc.php

--- src/templates/connection.inc.php
+++ src/templates/connection.inc.php
@@ -3,11 +3,11 @@
 //A déplacer à la racine du projet une fois rempli
 //Ne pas envoyer sur le github une fois rempli
 
-$servername = "mysql2.ouiheberg.com";
-$port = "3306";
-$username = "u3377_NwoQzma0Jn";
-$password = "5jvxEBJ@jRNLcZlCYMj.d3Hs";
-$dbname = "s3377_jadenn";
+$servername = "localhost";
+$port = "port";
+$username = "user";
+$password = "password";
+$dbname = "db name";
 
 try {
     $conn = new PDO("mysql:host=$servername;port=$port;dbname=$dbname", $username, $password);
