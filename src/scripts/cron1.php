057f650775a4ca86f77c28268bf62f580d9f5cc5
GameBlackRC <guillemetenzo2@gmail.com>
2024-02-27 11:06:50 +0100

    Récupération des vecteurs

--- src/scripts/cron1.php
+++ src/scripts/cron1.php
@@ -2,14 +2,14 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-$folder = "/home/xjeypbqy/poo-denis/user-profiles/";
-$file = $uid . ".json";
+//$folder = "/home/xjeypbqy/poo-denis/user-profiles/";
+//$file = $uid . ".json";
 
-$myfile = fopen($folder . $file, "a") or die("Unable to open file !");
-$json = "{['uid': N,'cosResult': X],['uid': N+1, 'cosResult': Y]}";
-fwrite($myfile, $txt);
+//$myfile = fopen($folder . $file, "a") or die("Unable to open file !");
+//$json = "{['uid': N,'cosResult': X],['uid': N+1, 'cosResult': Y]}";
+//fwrite($myfile, $txt);
 
-
+include("src/classes/Rating.php");
 
 function cosSimilarity($vector1, $vector2)
 {
@@ -34,7 +34,32 @@ function cosSimilarity($vector1, $vector2)
     return $similarity;
 }
 
+function getUsers($id_debut, $nb_user) {
 
+    $list = array();
+
+    global $conn;
+
+    $sql = "SELECT * FROM user WHERE id > $id_debut ORDER BY id LIMIT $nb_user";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    $tab = $stmt->fetchAll();
+    if (count($tab) > 0) {
+
+        foreach ($tab as $user) {
+            $id = $user['id'];
+            $list[$id] = Rating::getUserVector($id);
+        }
+
+        return $list;
+    } else {
+
+        return null;
+    }
+
+}
+
+var_dump(getUsers(0, 5));
 
 /*
 
